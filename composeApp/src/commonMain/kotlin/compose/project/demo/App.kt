5709e5653d69a0bad59bad9349060a35485aeafd
JonathanSnider <Jonathan.Snider@techconsulting.tech>
2025-01-09 16:43:03 -0500

    added basic navigation, praise the Lord

--- composeApp/src/commonMain/kotlin/compose/project/demo/App.kt
+++ composeApp/src/commonMain/kotlin/compose/project/demo/App.kt
@@ -1,136 +1,36 @@
 package compose.project.demo
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.material.Button
-import androidx.compose.material.DropdownMenu
-import androidx.compose.material.DropdownMenuItem
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
-import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import composedemo.composeapp.generated.resources.Res
-import composedemo.composeapp.generated.resources.eg
-import composedemo.composeapp.generated.resources.fr
-import composedemo.composeapp.generated.resources.id
-import composedemo.composeapp.generated.resources.jp
-import composedemo.composeapp.generated.resources.mx
-import kotlinx.datetime.Clock
-import kotlinx.datetime.IllegalTimeZoneException
-import kotlinx.datetime.LocalDateTime
-import kotlinx.datetime.LocalTime
-import kotlinx.datetime.TimeZone
-import kotlinx.datetime.toLocalDateTime
-import org.jetbrains.compose.resources.DrawableResource
-import org.jetbrains.compose.resources.painterResource
+import kotlinx.serialization.Serializable
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
-data class Country(val name: String, val zone: TimeZone, val image: DrawableResource)
-
-fun currentTimeAt(location: String, zone: TimeZone): String {
-    fun LocalTime.formatted() = "$hour:$minute:$second"
-
-    val time = Clock.System.now()
-    val localTime = time.toLocalDateTime(zone).time
-
-    return "The time in $location is ${localTime.formatted()}"
-}
-
-val defaultCountries = listOf(
-    Country("Japan", TimeZone.of("Asia/Tokyo"), Res.drawable.jp),
-    Country("France", TimeZone.of("Europe/Paris"), Res.drawable.fr),
-    Country("Mexico", TimeZone.of("America/Mexico_City"), Res.drawable.mx),
-    Country("Indonesia", TimeZone.of("Asia/Jakarta"), Res.drawable.id),
-    Country("Egypt", TimeZone.of("Africa/Cairo"), Res.drawable.eg)
-)
 
 @Composable
 @Preview
 fun App() {
-    val navController= rememberNavController()
-
-
-}
-
-fun todaysDate(): String {
-    fun LocalDateTime.format() = toString().substringBefore('T')
-
-    val now = Clock.System.now()
-    val zone = TimeZone.currentSystemDefault()
-    return now.toLocalDateTime(zone).format()
-}
-
-fun currentTimeAt(location: String): String? {
-    fun LocalTime.formatted() = "$hour:$minute:$second"
-
-    return try {
-        val time = Clock.System.now()
-        val zone = TimeZone.of(location)
-        val localTime = time.toLocalDateTime(zone).time
-        "The time in $location is ${localTime.formatted()}"
-    } catch (ex: IllegalTimeZoneException) {
-        null
-    }
-}
-
-@Composable
-fun TutorialBackUp(countries: List<Country> = defaultCountries) {
-    MaterialTheme {
-        var showCountries by remember { mutableStateOf(false) }
-        var timeAtLocation by remember { mutableStateOf("No location selected") }
-
-        Column(modifier = Modifier.padding(20.dp)) {
-            Text(
-                timeAtLocation,
-                style = TextStyle(fontSize = 20.sp),
-                textAlign = TextAlign.Center,
-                modifier = Modifier.fillMaxWidth().align(Alignment.CenterHorizontally)
+    val navController = rememberNavController()
+    NavHost(
+        navController = navController,
+        startDestination = RoomListRoute
+    ) {
+        composable<RoomListRoute> {
+            RoomScreen(
+                navigateToPeopleScreen = { navController.navigate(PeopleListRoute) }
             )
-            Row(modifier = Modifier.padding(start = 20.dp, top = 10.dp)) {
-                DropdownMenu(
-                    expanded = showCountries,
-                    onDismissRequest = { showCountries = false }
-                ) {
-                    countries.forEach { (name, zone, image) ->
-                        DropdownMenuItem(
-                            onClick = {
-                                timeAtLocation = currentTimeAt(name, zone)
-                                showCountries = false
-                            }
-                        ) {
-                            Row(verticalAlignment = Alignment.CenterVertically) {
-                                Image(
-                                    painterResource(image),
-                                    modifier = Modifier.size(50.dp).padding(end = 10.dp),
-                                    contentDescription = "$name flag"
-                                )
-                                Text(name)
-                            }
-                        }
-                    }
-                }
-            }
-
-            Button(modifier = Modifier.padding(start = 20.dp, top = 10.dp),
-                onClick = { showCountries = !showCountries }) {
-                Text("Select Location")
-            }
+        }
+        composable<PeopleListRoute> {
+            PeopleScreen(
+                navigateToRoomScreen = { navController.navigate(RoomListRoute) }
+            )
         }
     }
 }
+
+@Serializable
+object RoomListRoute
+
+@Serializable
+object PeopleListRoute
---
+++ composeApp/src/commonMain/kotlin/compose/project/demo/RoomScreen.kt
@@ -0,0 +1,26 @@
+package compose.project.demo
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun RoomScreen(
+    navigateToPeopleScreen: () -> Unit
+) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize(),
+        contentAlignment = Alignment.BottomCenter
+    ) {
+        Button(
+            onClick = navigateToPeopleScreen
+        ){
+            Text("Go To People Screen")
+        }
+    }
+}
